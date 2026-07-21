ce9568fd49308c664494d2be060057f03265ccd7
Yusuf-El-sheikh <yusuf.elsheikh01@gmail.com>
2026-07-22 01:22:32 +0300

    finished homework!!!

--- part1.js
+++ part1.js
@@ -195,3 +195,86 @@ function p11(...values)
 
 // problem - 12
 
+function p12()
+{
+  return new Promise((resolve) => {
+    setTimeout(() =>   
+    {
+      resolve("success");
+    },3000);
+  });
+}
+
+//p12().then((message) => {console.log(message);}) // i did NOT like this part (promises syntax wise)
+
+// problem - 13
+
+function p13()
+{
+  let arr = [1, 3, 7, 2, 4];
+
+  let max = -1;
+
+  for(let i = 0 ; i < arr.length ; i++)
+  {
+    if(arr[i] > max)
+      {
+        max = arr[i] ;
+      }
+  }
+
+  return max;
+}
+
+//console.log(p13());
+
+// problem - 14
+
+function p14(obj)
+{
+  let arr = [];
+
+  for(key in obj)
+  {
+    arr.push(key)
+  }
+
+  return arr;
+}
+
+//console.log(p14({name: "John", age: 30}));
+
+// problem - 15
+
+function p15(string)
+{
+  arr = string.split(" ");
+
+  return arr;
+}
+
+function anotherSol(string)
+{
+  let temp = "";
+  let arr = [];
+  
+  for(let i = 0 ; i < string.length ; i++)
+  {
+    if(string[i] != " ")
+    {
+      temp += string[i];
+    }
+
+    else
+    {
+      arr.push(temp);
+      temp = "";
+    }
+  }
+
+  arr.push(temp);
+
+  return arr;
+}
+//console.log(p15("The quick brown fox"));
+//console.log(anotherSol("The quick brown fox"));
